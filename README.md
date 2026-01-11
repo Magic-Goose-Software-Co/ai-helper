d96aadeeef42e8f0a495af1e433a44ee14f0edc1
Jack Huey <jhhuey@icloud.com>
2026-01-11 15:10:21 -0600

    Original files

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# ai-helper
+
+A small Python helper library for working with artificial intelligence providers using the OpenAI schema
+
+---
+
+
+## Installation
+
+```bash
+pip install git+https://git.jackhuey.com/ai-helper.git
+```
+
+---
+
+## Usage
+
+### Basic usage:
+```python
+import ai
+
+openai = ai.Provider("your-api-key")
+
+model = ai.AI(provider, "You are a helpful AI assistant.")
+
+while True:
+    prompt = input("You: ")
+    response = model.prompt(prompt)
+    print(response)
+```
+---
+
+### Using a different provider
+If you want to use a different provider (e.g. GitHub models), pass it as a string to ai.Provider.
+
+```python
+github = ai.Provider("your-api-key", baseUrl="https://models.github.ai/inference")
+```
+### Tool Calls
+To enable tool calls, pass a list of tool definitions to ai.AI.
+If a tool takes no parameters, you may pass an empty list.
+
+```python
+model = ai.AI(provider, "You are a helpful AI assistant.",
+    {
+        "name": "web",
+        "description": "Browse the web.",
+        "parameters": [
+            {
+                "url": {
+                    "type": "string",
+                    "description": "The URL of the page you want to visit."
+                }
+            }
+        ]
+    }
+)
+```
+If the model uses a tool call, ai.AI.prompt will return a list of instances of ai.AI.ToolCall.
+
+ai.AI.ToolCall has the attributes name, a string of the name (e.g. "web" from the example above), and arguments, a dictionary of the parameters passed (e.g. {"url": "google.com"} from the example above).
+
+ai.AI.ToolCall has the method respond(), which takes one parameter, the response as a string. You must call respond for all tool calls returned by ai.AI.prompt.
---
+++ src/ai/__init__.py
@@ -0,0 +1,58 @@
+from openai import OpenAI
+import ast
+
+class Provider:
+    def __init__(self, apiKey, baseUrl=None):
+        self.apiKey = apiKey
+        self.baseUrl = baseUrl
+
+class AI:
+    def __init__(self, provider, systemPrompt, tools=[]):
+        if provider.baseUrl:
+            self.client = OpenAI(api_key=provider.apiKey, base_url=provider.baseUrl)
+        else:
+            self.client = OpenAI(api_key=provider.apiKey)
+        self.history = [{"role": "system", "content": systemPrompt}]
+        self.tools = []
+
+        for tool in tools:
+            parameters = {}
+            for parameter in tool["parameters"]:
+                parameters = {
+                    "type": "object",
+                    "properties": {
+                        parameter: {"type": tool["parameters"][parameter]["type"], "description": tool["parameters"][parameter]["description"]}
+                    },
+                "required": list(tool["parameters"].keys())
+                }
+
+            self.tools.append({
+            "type":"function",
+            "function":{
+                "name": tool["name"],
+                "description": tool["description"],
+                "parameters": parameters
+            }
+        })
+
+    class ToolCall:
+        def __init__(self, model, name, arguments, id):
+            self.model = model
+            self.name = name
+            self.arguments = ast.literal_eval(arguments)
+            self.id = id
+
+        def respond(self, response):
+            self.model.history.append({"role": "tool", "tool_call_id": self.id, "content": response})
+
+    def prompt(self, prompt):
+        self.history.append({"role": "user", "content": prompt})
+        response = self.client.chat.completions.create(model="gpt-4.1-mini", messages=self.history, tools=self.tools).choices[0].message
+        if response.tool_calls:
+            self.history.append({"role": "assistant", "tool_calls": [tc.model_dump() for tc in response.tool_calls], "content": None})
+            return [self.ToolCall(self, toolCall.function.name, toolCall.function.arguments, toolCall.id) for toolCall in response.tool_calls]
+        else:
+            self.history.append({"role": "assistant", "content": response.content})
+            return response.content
+
+
